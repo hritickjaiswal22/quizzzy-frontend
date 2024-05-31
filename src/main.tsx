5f556c05bdab8e07078b04a842ed92f3422738e7
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2024-05-31 14:24:16 +0530

    frontend using graphql

--- src/main.tsx
+++ src/main.tsx
@@ -7,21 +7,16 @@ import grapgqlClient from "./grapgqlClient.ts";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "@/features/ThemeProvider.tsx";
-import { QueryClient, QueryClientProvider } from "react-query";
 import { ApolloProvider } from "@apollo/client";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <ApolloProvider client={grapgqlClient}>
-      <QueryClientProvider client={queryClient}>
-        <Provider store={store}>
-          <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-            <App />
-          </ThemeProvider>
-        </Provider>
-      </QueryClientProvider>
+      <Provider store={store}>
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+          <App />
+        </ThemeProvider>
+      </Provider>
     </ApolloProvider>
   </BrowserRouter>
 );
